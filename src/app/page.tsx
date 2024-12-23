f351d909da61c74eaac5cc41d1150e5832483921
Diana Araujo G <dianac251@gmail.com>
2024-12-23 09:14:14 -0600

    UPDATE(page): The dashboard components were arranged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,77 @@
-import Image from "next/image";
+'use client'
+
+import React, { useEffect } from "react";
+import { useMxStateState } from "./context/mxState";
+import { useWeather } from "./hooks/useWeather";
+import TemperatureCard from "./ui/Cards/Temperature";
+import NavBar from "./ui/NavBar";
+import SearchSelect from "./ui/SearchSelect";
+import Sidebar from "./ui/Sidebar";
+import PollutionCard from "./ui/Cards/Pollution";
+import PredictionCard from "./ui/Cards/Prediction";
+import Loader from "./ui/Loader";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const MxState = useMxStateState();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+ const {
+    getStateMX,
+    getWeatherByCity, 
+    getPollutionByCords, 
+    getWeatherByCityForSomeDays,
+    isLoadingWeatherByCityForSomeDays,
+    isLoadingPollutionByCords,
+    isLoadingWeatherByCity,
+    listStatesMx,
+    parseIConUrl,
+    parseDataCityWeather, 
+    parseDataPollutionByCords, 
+    parseWeatherForSomeDays,
+    firstDateWeather
+  } = useWeather();
+
+  useEffect(()=> {
+    getStateMX()
+  },[])
+
+  useEffect(()=> {
+    if(MxState.city.length){
+      getWeatherByCity(MxState.city);
+      getWeatherByCityForSomeDays(MxState.city);
+    }
+  }, [MxState.city])
+
+  useEffect(()=>{
+    if(MxState.coord){
+      getPollutionByCords(MxState.coord.lon, MxState.coord.lat);
+    }
+  }, [MxState.coord])
+
+  return (
+    <div className="min-h-screenfont-[family-name:var(--font-geist-sans)]">
+      <NavBar />
+      <Sidebar />
+      <main className="flex flex-wrap flex-col items-center md:items-start p-4 gap-16 md:ml-64 sm:p-20 ">
+        <div className="w-full flex flex-wrap  flex-col items-center justify-center ">
+          <SearchSelect optionsStates={listStatesMx} />
+          {
+            isLoadingWeatherByCityForSomeDays || isLoadingPollutionByCords || isLoadingWeatherByCity ?
+            <Loader />
+            :
+            <>
+              <div className="flex flex-col lg:flex-row gap-4 mt-4">
+                <TemperatureCard data={parseDataCityWeather} iconUrl={parseDataCityWeather.icon.length ? parseIConUrl(parseDataCityWeather.icon) : ''}/>
+                <PollutionCard data={parseDataPollutionByCords} />
+              </div>
+              <PredictionCard 
+                data={parseWeatherForSomeDays} 
+                firstDate={firstDateWeather} 
+                parseIConUrl={parseIConUrl}
+              />
+            </>
+          }
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
